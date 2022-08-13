3893c957155761a52bbffcc35f1cfb7137012d74
Evgeniy <241126@mail.ru>
2022-08-13 03:43:17 +0300

    Finish code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество строк ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = new string[size];
+int b = 0;
+
+for (int i = 0; i < size; i++)
+{
+    Console.Write("Введите строку: ");
+    string a = Console.ReadLine();
+
+    if (a.Length <=3)
+    {
+        array[b] = a;
+        b++;
+    }
+}
+
+
+Console.Write($"Ответ: [");
+OutputArray(array);
+Console.Write("]");
+
+
+void OutputArray(string[] array)
+{
+    for (int i = 0; i < b; i++)
+    {
+        Console.Write($"{array[i]}");
+        if (i < (b-1))
+        {
+            Console.Write($", ");
+        }
+    }
+}
